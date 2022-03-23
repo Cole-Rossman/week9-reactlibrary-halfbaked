77fb6d999df15b91469cefc606df3863f84ba7b5
Cole Rossman <colerossman99@gmail.com>
2022-03-23 11:09:04 -0700

    BrowseRouter implemented in App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import BookList from './views/Books/BookList';
+import BookDetail from './views/Books/BookDetail';
 import './App.css';
+
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+
 
 function App() {
   // TODO: Add routes to books & views
   return (
-    <main className="container">
-      <h1>Library Catalog</h1>
-      <BookList />
-    </main>
+    <BrowserRouter>
+      <main className="container">
+        <h1>Library Catalog</h1>
+        <Switch>
+          <Route exact path="/books">
+            <BookList />
+          </Route>
+          <Route path="/books/:id">
+            <BookDetail />
+          </Route>
+        </Switch>
+      </main>
+    </BrowserRouter>
   );
 }
 
